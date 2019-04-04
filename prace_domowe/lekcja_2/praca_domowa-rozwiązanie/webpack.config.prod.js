d2862622d70eca2d13a196fdb00ca4a3b14cc21c
Piotr Martyniak <piotr.martyniak@daftcode.pl>
2019-04-04 14:07:52 +0200

    feat(lekcja2): zadanie domowe - rozwiazanie

---
+++ prace_domowe/lekcja_2/praca_domowa-rozwiązanie/webpack.config.prod.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const distDir = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: distDir
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin([
+      { from: path.resolve(__dirname, 'src/img'), to: path.resolve(distDir, 'img') },
+    ])
+  ]
+};
